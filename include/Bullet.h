a1615b9cade37671c6fd193e6de47cc4ac451e97
Fabian Wiidh <fabian.wiidh@gmail.com>
2022-12-16 16:22:45 +0100

    Update

--- include/Bullet.h
+++ include/Bullet.h
@@ -1,10 +1,13 @@
 ﻿#ifndef BULLET_H
 #define BULLET_H
 
+#include "Sprite.h"
+#include <string>
+
 class Bullet : public Sprite{
 
     Bullet(int, int, int, int, std::string);    
 
-}
+};
 
 #endif
