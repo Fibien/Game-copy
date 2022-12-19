6c93de2bcd475bdd2aa5f1da81a8fa086c0514ce
Fibien <fabian.wiidh@gmail.com>
2022-12-19 20:05:22 +0100

    Update

--- src/Sprite.cpp
+++ src/Sprite.cpp
@@ -5,6 +5,4 @@
 #include "System.h"
 #include "Constants.h"
 
-// Ska lägga till värdena i rektangeln
-
  Sprite::Sprite(int x, int y, int w, int h, SDL_Texture* texture_) : rect_{x,y,w,h}, texture_(texture_) {}
