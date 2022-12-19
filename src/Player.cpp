9a841913d6d9cb15f40e77aa318f9506e6ca23bb
Erik Olofsson <playde5555@gmail.com>
2022-12-19 10:37:38 +0100

    space

--- src/Player.cpp
+++ src/Player.cpp
@@ -18,6 +18,8 @@ Player* Player::getInstance(int x, int y, int h, int w, SDL_Texture* texture) {
 void Player::keyDown(SDL_Event& eve, int max_x_){
 
     int move = 5;
+    
+
 
     if (eve.key.keysym.sym == SDLK_LEFT && this->getRect().x >= move) {
         this->getRect().x -= move; 
